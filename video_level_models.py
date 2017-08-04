a6bb70c1fb5471f5fc0cb415b1e8633798f1eb4d
Isaac Julien <isaacjulien@Isaacs-MacBook-Pro.local>
2017-08-03 20:04:49 -0700

    densenet test

--- video_level_models.py
+++ video_level_models.py
@@ -283,9 +283,9 @@ class IsaacNet(models.BaseModel):
     self.reduction = 1.0
     self.bc_mode = True
 
-    depth = 60
+    depth = 100
     total_blocks = 3
-    growth_rate = 20
+    growth_rate = 12
 
     self.n_classes = vocab_size
 
